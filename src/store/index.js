48291faf6ae3abd64919d195075d1d047d7e18b9
Azamjon <akbaraliev0707@gmail.com>
2022-09-26 14:51:29 +0900

    outputting incoming requests

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
-import coachModule from './modules/coaches/index.js';
+import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
 
 const store = createStore({
   modules: {
-    coaches: coachModule,
+    coaches: coachesModule,
+    requests: requestsModule,
   },
   state() {
     return {
